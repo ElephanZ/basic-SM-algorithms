ecf35cb34ba7d9607dc196f578add73e55ac72d0
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2021-01-20 12:08:52 +0100

    Update hor.h

--- src/hor.h
+++ src/hor.h
@@ -1,17 +1,17 @@
 #pragma once
 
-void horspool_preprocessing(string P, uint M, uint *hbc)
+void horspool_preprocessing(const string P, const uint M, uint *hbc)
 {
    for (uint i = 0; i < SIGMA_SIZE; i++) hbc[i] = M;
    for (uint i = 0; i < M - 1; i++) hbc[(uint)P[i]] = M - i - 1;
 }
 
-uint horspool(string T, uint N, string P, uint M)
+uint horspool(const string T, const uint N, const string P, const uint M)
 {
    uint cnt = 0;
    uint *hbc = new uint[SIGMA_SIZE];
 
-   auto check = [=](uint s)
+   auto check = [=](const uint s)
    {
       uint j = 0;
       while (j < M && T[s + j] == P[j]) j++;
