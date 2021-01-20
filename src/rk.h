ead14f6fc4b6826942c7fa18289385c5fb54a3cf
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2021-01-20 12:13:05 +0100

    Update rk.h

--- src/rk.h
+++ src/rk.h
@@ -1,8 +1,8 @@
 #pragma once
 
-void rabinKarp_preprocessing(string P, string T, uint M, uint &hash, uint &t_hash, uint &p_hash)
+void rabinKarp_preprocessing(const string P, const string T, const uint M, uint &hash, uint &t_hash, uint &p_hash)
 {
-   for (uint i = 1; i < M; i++) hash = (hash << 1);
+   for (uint i = 1; i < M; i++, hash <<= 1);
    for (uint i = 0; i < M; i++)
       p_hash = ((p_hash << 1) + (uint)P[i]),
       t_hash = ((t_hash << 1) + (uint)T[i]);
@@ -12,15 +12,12 @@ uint rabinKarp(string T, uint N, string P, uint M)
 {
    uint h = 1, hp = 0, ht = 0, cnt = 0;
 
+   auto rehash_t = [&](uint s) { return ((ht - T[s]*h) << 1) + T[M + s]; };
    auto check = [=](uint s)
    {
       uint j = 0;
       while (j < M && T[s + j] == P[j]) j++;
       return j >= M;
-   };
-   auto rehash_t = [&](uint s)
-   {
-      return ((ht - T[s]*h) << 1) + T[M + s];
    };
 
    rabinKarp_preprocessing(P, T, M, h, ht, hp);
